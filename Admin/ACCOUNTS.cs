f7d8bbe06c03c5304ff2a159f8b1b1ad1a592a05
Ragrats- <91531302+ragrats1403@users.noreply.github.com>
2021-11-22 17:25:46 +0800

    [Backend] Rounded Corners on Form

--- Admin/ACCOUNTS.cs
+++ Admin/ACCOUNTS.cs
@@ -8,18 +8,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Runtime.InteropServices;
 
 namespace Function_Hall_Reservation_System.Admin
 {
     public partial class ACCOUNTS : Form
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // width of ellipse
+            int nHeightEllipse // height of ellipse
+        );
         public ACCOUNTS()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
         }
         private void ACCOUNTS_Load(object sender, EventArgs e)
         {
             Filldata();
+            
             lblfullname.Text = Form1.setfullname;
         }
 
